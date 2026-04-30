6116c483fc56e966290e79cd031be9d8b3454886
“Wesley <wesleyfr.sobrinho@gmail.com>
2026-04-30 10:16:30 -0300

    feat: estrutura inicial (scaffold)

--- app/src/main/java/com/anotasmart/MainActivity.kt
+++ app/src/main/java/com/anotasmart/MainActivity.kt
@@ -4,13 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.Inventory
+import androidx.compose.material.icons.filled.ListAlt
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingBag
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.anotasmart.ui.theme.AnotaSmartTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +53,163 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AnotaSmartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScreenStructure()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ScreenStructure() {
+    var quantidadeItens by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            BarraSuperiorAnotaSmart(quantidadeItens = quantidadeItens)
+        },
+        bottomBar = {
+            Column {
+                if (quantidadeItens > 0) {
+                    ResumoCarrinho(quantidadeItens = quantidadeItens)
+                }
+                BarraNavegacaoPrincipal()
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(Color.White)
+        ) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BarraSuperiorAnotaSmart(quantidadeItens: Int) {
+    CenterAlignedTopAppBar(
+        title = {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Perfil do Usuário"
+                )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Navigation drawer"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { }) {
+                BadgedBox(
+                    badge = {
+                        if (quantidadeItens > 0) {
+                            Badge {
+                                Text(text = quantidadeItens.toString())
+                            }
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ShoppingCart,
+                        contentDescription = "Carrinho de Compras"
+                    )
+                }
+            }
+        }
     )
+}
+
+@Composable
+fun ResumoCarrinho(quantidadeItens: Int) {
+    Surface(
+        tonalElevation = 8.dp,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(
+                    text = "TOTAL: R$ 51,00",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = "Quantidade: $quantidadeItens",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            Button(onClick = { }) {
+                Text("VER CARRINHO")
+            }
+        }
+    }
+}
+
+@Composable
+fun BarraNavegacaoPrincipal() {
+    NavigationBar {
+        NavigationBarItem(
+            selected = true,
+            onClick = { },
+            icon = { Icon(Icons.Default.ShoppingBag, contentDescription = "Venda") },
+            label = { Text("Venda") },
+            alwaysShowLabel = false
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(Icons.Default.Inventory, contentDescription = "Produtos") },
+            label = { Text("Produtos") },
+            alwaysShowLabel = false
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(Icons.Default.ListAlt, contentDescription = "Pedidos") },
+            label = { Text("Pedidos") },
+            alwaysShowLabel = false
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(Icons.Default.People, contentDescription = "Clientes") },
+            label = { Text("Clientes") },
+            alwaysShowLabel = false
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(Icons.Default.AttachMoney, contentDescription = "Despesas") },
+            label = { Text("Despesas") },
+            alwaysShowLabel = false
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(Icons.Default.MoreHoriz, contentDescription = "Mais") },
+            label = { Text("Mais") },
+            alwaysShowLabel = false
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AnotaSmartTheme {
-        Greeting("Android")
-    }
+fun Preview(){
+    BarraSuperiorAnotaSmart(1)
+    ResumoCarrinho(1)
 }
